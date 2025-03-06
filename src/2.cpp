69b38940889233a26de86ce08e2b97929309d9fc
bennieschhr <bennieschhr@gmx.com>
2025-03-06 17:48:27 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Welcome to Math Homework" << std::endl;
+    int a = 5;
+    int b = 8;
+    int c = a * b;
+    std::cout << "The result is: " << c << std::endl;
+    return 0;
+}
