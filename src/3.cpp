a3c80144b449e784fc4f12d5288fcebd1766bd4d
bennieschhr <bennieschhr@gmx.com>
2025-03-07 09:27:58 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  int x = 4;
+  float y = 3.14;
+  double z = 2.71828;
+
+  std::cout << "The value of x is: " << x << std::endl;
+  std::cout << "The value of y is: " << y << std::endl;
+  std::cout << "The value of z is: " << z << std::endl;
+
+  return 0;
+}
